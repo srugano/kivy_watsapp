b362341fe29e9df50c96ff82131d4df5a9c8eeb2
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2014-08-31 10:01:23 +0200

    added objects

--- orkiv/__main__.py
+++ orkiv/__main__.py
@@ -1,11 +1,19 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
  
  
 class Orkiv(App):
     pass
 
 class AccountDetailsForm(AnchorLayout):
-    pass
+    server_box = ObjectProperty()
+    username_box = ObjectProperty()
+    password_box = ObjectProperty()
+
+    def login(self):
+        print(self.server_box.text)
+        print(self.username_box.text)
+        print(self.password_box.text)
  
 Orkiv().run()
